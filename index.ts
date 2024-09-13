3a05fb8d63f4e219a91cbe234518aca5b752fd86
Nistha <nistha984475@gmail.com>
2024-09-13 14:56:11 +0545

    adds string reversal function

---
+++ index.ts
@@ -0,0 +1,11 @@
+function reverseString(word: string) : string{
+  let reversedString ="";
+  if (!word) return "word is empty";
+  for (let i = word.length - 1; i >= 0; i--) {
+    reversedString += word[i];
+  }
+  return reversedString;
+}
+
+const word = "Reverse this sentence";
+console.log(reverseString(word));
