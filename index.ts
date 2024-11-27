d8f6f27cc28b08c63c3c263a3f4346f28e8894bb
poudelkaushal <paudelkaushal@gmail.com>
2024-11-27 13:10:42 +0545

    .

--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
-function reverseString(word: string) : string{
-  let reversedString ="";
+function reverseString(word: string): string {
+  let reversedString = "";
   if (!word) return "word is empty";
   for (let i = word.length - 1; i >= 0; i--) {
     reversedString += word[i];
@@ -7,5 +7,5 @@ function reverseString(word: string) : string{
   return reversedString;
 }
 
-const word = "Reverse this sentence";
+const word = "Reverse this sentence.";
 console.log(reverseString(word));
